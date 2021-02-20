a0bf0e8cc0b082a1d6e3fe5c659fc7acece0656f
YYZYHS <77320657+YYZYHS@users.noreply.github.com>
2021-02-20 17:19:10 +0800

    Study_OpenCV

---
+++ Year-2021/Study_Note/Qt/Code/opencv/opencvtext/main.cpp
@@ -0,0 +1,22 @@
+﻿#include "mainwindow.h"
+#include <QApplication>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace cv;
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    cv::Mat image = imread("C:\\Users\\yyz\\Desktop1.jpg");
+    namedWindow("Display window",WINDOW_AUTOSIZE);
+    imshow("Display window",image);
+    waitKey(0);
+
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
